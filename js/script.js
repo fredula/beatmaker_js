e4ed476e6e0017483f28e25f379f31dae4cae023
fredula <fred.thefriedone@gmail.com>
2017-01-04 22:48:04 +0200

    changed animation to requestanimationframe, styling changes

--- js/script.js
+++ js/script.js
@@ -8,6 +8,11 @@ var app = function(){
 	let leftPos = 200;
 	let counter = 0;
 	let timer;
+	let fps = 4;
+	let now;
+	let then = Date.now();
+	let interval = 1000/fps;
+	let delta;
 
 	function clickHandler(e) {
 		if(e.target.tagName.toLowerCase() === 'button') {
@@ -26,35 +31,38 @@ var app = function(){
 	  audio.play();
 	}
 
-	function doBeat() {
-		clearTimeout(timer);
-		counter++;
-		if(counter < 17) {
-			var currIndicator = document.querySelector('.indicator_' + counter);
-			currIndicator.style.opacity = 0.1;
-			currIndicator.out = setTimeout(function(){ currIndicator.style.opacity = 0; }, 250);
-			rows.forEach(row => {
-				if(row.children[counter].classList.contains('active')) {
-					playSound(row.children[counter].getAttribute('data-key'));
-					//console.log(row.children[counter].getAttribute('data-key'));
-				}
-		 		//console.log(row.children[counter].classList.contains('active'));
-		 	})
-			startBeats();
-		}else{
-			counter = 0;
-			startBeats();
-		}
-	}
+	function playBeat() {
+	     
+	    requestAnimationFrame(playBeat);
+	     
+	    now = Date.now();
+	    delta = now - then;
+  
+	    if (delta > interval) {
+	         
+	        then = now - (delta % interval);
 
-	function startBeats() {
-		timer = setTimeout(function() {
-			doBeat();
-		}, 250) 
+	        if(counter <= 16) {
+				var currIndicator = counter == 0 ? document.querySelector('.indicator_1') : document.querySelector('.indicator_' + counter);
+				currIndicator.style.opacity = 0.1;
+				currIndicator.out = setTimeout(function(){ currIndicator.style.opacity = 0; }, interval);
+				rows.forEach(row => {
+					if(row.children[counter].classList.contains('active')) {
+						playSound(row.children[counter].getAttribute('data-key'));
+						//console.log(row.children[counter].getAttribute('data-key'));
+					}
+			 		//console.log(row.children[counter].classList.contains('active'));
+			 	})
+			 	//this takes care of the delay after the last beat is played
+			 	counter == 16 ? counter = 1 : counter++;
+			}else{
+				counter = 1;
+			}
+	    }
 	}
 	
 	buttons.forEach(button => button.addEventListener('click', clickHandler));
-	playBut.addEventListener('click', startBeats);
+	playBut.addEventListener('click', playBeat);
 };
 
 document.addEventListener('DOMContentLoaded', function() {
