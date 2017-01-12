3daa15063936508328dfca2b227762aeaba847fa
fredula <fred.thefriedone@gmail.com>
2017-01-12 22:40:03 +0200

    updates to selectors, added stop button

--- js/script.js
+++ js/script.js
@@ -5,7 +5,33 @@ var app = function(){
 	const buttons = document.querySelectorAll('.beatmaker__beat');
 	const indicator = document.querySelector('.beatmaker__indicator');
 	const playBut = document.querySelector('.beatmaker__play');
+	const stopBut = document.querySelector('.beatmaker__stop');
 	const bpmSelector = document.querySelector('.beatmaker__bpm');
+	const preSelector = document.querySelector('.beatmaker_pre');
+	const predefinedBeats = [
+								{
+									1:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1],
+									2:[0,0,0,0,1,0,0,0,0,0,0,1,1,0,0,0,0],
+									3:[0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0],
+									4:[0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0],
+									5:[0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0],
+									6:[0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0],
+									7:[0,0,0,1,0,0,0,1,1,0,0,1,0,0,0,1,1],
+									8:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+									9:[0,0,0,0,1,0,0,0,0,0,0,0,1,1,1,0,0],
+								},
+								{
+									1:[0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0],
+									2:[0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0],
+									3:[0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0],
+									4:[0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,0],
+									5:[0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0],
+									6:[0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0],
+									7:[0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0],
+									8:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1],
+									9:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1],
+								}
+							]
 	let leftPos = 200;
 	let counter = 0;
 	let timer;
@@ -14,6 +40,8 @@ var app = function(){
 	let then = Date.now();
 	let interval;
 	let delta;
+	let isPlaying = false;
+	let req;
 
 	function clickHandler(e) {
 		if(e.target.tagName.toLowerCase() === 'button') {
@@ -33,8 +61,18 @@ var app = function(){
 	}
 
 	function playBeat() {
-	    requestAnimationFrame(playBeat);
+	    req = requestAnimationFrame(playBeat);
 	    
+	    if(this){
+	    	if(!isPlaying) {
+	    		this.style.display = "none";
+	    		stopBut.style.display = "block";
+	    		bpmSelector.setAttribute('disabled', true);
+	    		preSelector.setAttribute('disabled', true);
+	    		isPlaying = true;
+	    	}
+	    }
+
 	    interval = 1000/fps;
 	    now = Date.now();
 	    delta = now - then;
@@ -79,10 +117,41 @@ var app = function(){
 		var currentBPM = el.options[el.selectedIndex].dataset.bpm;
 		setBPM(currentBPM);
 	}
+
+	function handlePreChange(e) {
+		var el = e.target;
+		var currentPRE = el.options[el.selectedIndex].dataset.pre;
+		if (currentPRE === Number(1)) setBPM('120');
+		var rows = document.querySelectorAll('.beatmaker__row');
+		var counter = 1;
+		rows.forEach(function(el) {
+			var child = el.children;
+			[].forEach.call(child, function(childEl, index) {
+				if(predefinedBeats[Number(currentPRE)][counter][index] === 1) child[index].classList.add('active');
+			})
+			counter++
+		})
+	}
+
+	function stopBeat() {
+		window.cancelAnimationFrame(req);
+		if(isPlaying) {
+    		this.style.display = "none";
+    		playBut.style.display = "block";
+    		isPlaying = false;
+    		bpmSelector.removeAttribute('disabled');
+	    	preSelector.removeAttribute('disabled');
+    	}
+	}
+	// setTimeout(function() {
+	// 	window.cancelAnimationFrame(req);
+	// },5000);
 	
 	buttons.forEach(button => button.addEventListener('click', clickHandler));
 	playBut.addEventListener('click', playBeat);
+	stopBut.addEventListener('click', stopBeat);
 	bpmSelector.addEventListener('change', handleBPMChange);
+	preSelector.addEventListener('change', handlePreChange);
 };
 
 document.addEventListener('DOMContentLoaded', function() {
